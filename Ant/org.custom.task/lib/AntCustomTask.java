802b9a454ad7286db74949c603cbfcc85a9353eb
taniuchi.wataru <taniuchi.wataru@unisoft.co.jp>
2017-04-24 13:44:23 +0900

    L1,L7 コメント追加

--- Ant/org.custom.task/lib/AntCustomTask.java
+++ Ant/org.custom.task/lib/AntCustomTask.java
@@ -1,10 +1,10 @@
-package org.custom.ant.task
+package org.custom.ant.task //疑似パッケージ
 
 import org.apache.tools.ant.Task; // apacheAntをimport
 
 public class AntCustomTask extends Task{	// Taskクラスを継承
 
-	private String name;
+	private String name; // タグのオプションになる
 
 	public class execute() throws BuildException{
 		//処理
